bea4a2ddcce619620f86c8521945d85a3c2ecb41
Huiyang Chen <huiyangc@umich.edu>
2025-04-06 20:27:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('newsletterForm');
+
+    if (form) {
+      console.log('Form found!');
+      form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Successfully Submitted!');
+      });
+    } else {
+      console.log('Form not found!');
+    }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const select = document.getElementById("mySelect");
+    const output = document.getElementById("demo");
+
+    select.addEventListener("change", function () {
+        output.textContent = "You selected: " + select.value;
+    });
+});
+
